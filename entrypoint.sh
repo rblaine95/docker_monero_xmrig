a11beb5d76939a85eca4cf0278d87e623d06a2e6
Robbie Blaine <robbie@zenaptix.com>
2018-04-11 11:28:08 +0200

    Entrypoint commit

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,55 +2,54 @@
 
 sysctl vm.nr_hugepages=128
 
-if [ -z "$numthreads" ]
-then
-      echo "\$numthreads is empty"
-      cache=$(cat /proc/cpuinfo | grep "cache size" | awk 'NR==1{print $4}')
-      numthreads=$(expr $cache / 2048)
+if [ -z "$numthreads" ]; then
+    echo "\$numthreads is empty"
+    cache=$(cat /proc/cpuinfo | grep "cache size" | awk 'NR==1{print $4}')
+    numthreads=$(expr $cache / 2048)
 else
-      echo "\$numthreads is NOT empty"
+    echo "\$numthreads is NOT empty"
+fi
+echo Using --- $numthreads
+
+if [ -z "$startport" ] ;then
+    echo "\$startport is empty"
+    startport=8100
+else
+    echo "\$startport is NOT empty"
+fi
+echo Using --- $startport
+
+if [ -z "$xmrpool" ]; then
+    echo "\$xmrpool is empty"
+    xmrpool=xmr-usa.dwarfpool.com
+else
+    echo "\$xmrpool is NOT empty"
+fi
+echo Using --- $xmrpool
+
+if [ -z "$password" ]; then
+    echo "\$password is empty"
+    password=x
+    echo Using --- $password
+else
+    echo "\$password is NOT empty"
 fi
 
-if [ -z "$startport" ]
-then
-      echo "\$startport is empty"
-      startport=8100
+if [ -z "$donate" ]; then
+    echo "\$donate is empty"
+    donate=5
 else
-      echo "\$startport is NOT empty"
+    echo "\$donate is NOT empty"
+    if [ "$donate" -lt 1 ]; then
+        echo "\$donate must be greater than or equal to 1"
+        donate=1
+    fi
 fi
+echo Using --- $donate
 
-
-if [ -z "$xmrpool" ]
-then
-      echo "\$xmrpool is empty"
-      xmrpool=xmr-usa.dwarfpool.com
-      echo Using $xmrpool
-else
-      echo "\$xmrpool is NOT empty"
-      echo Using --- $xmrpool
-fi
-
-if [ -z "$password" ]
-then
-      echo "\$password is empty"
-      password=x
-      echo Using --- $password
-else
-      echo "\$password is NOT empty"
-fi
-if [ -z "$donate" ]
-then
-      echo "\$donate is empty"
-      donate=5
-      echo Using --- $donate
-else
-      echo "\$donate is NOT empty"
-fi
-
-#wget http://files.linux-bench.com/mine/xmrig.tar.gz
-#tar xzvf xmrig.tar.gz
 git clone https://github.com/xmrig/xmrig.git
 cd xmrig
+git checkout v$VERSION
 mkdir build
 cd build
 cmake ..
